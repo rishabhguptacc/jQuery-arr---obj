583f9a050525c643717ee6e430d7e5b2a64e046a
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-03-02 11:51:56 +0530

    Dynamically added all the files to HTML page.

--- src/products.js
+++ src/products.js
@@ -6,6 +6,42 @@ var products = [{"id":101,"name":"Basket Ball","image":"basketball.png","price":
 
 
 $(document).ready(function(){
-    // displayProducts();
+    displayProducts();
 });
 
+function displayProducts(){
+    html = "";
+
+    html += '<div id="product-101" class="product">\
+            <img src="images/football.png">\
+            <h3 class="title"><a href="#">Product 101</a></h3>\
+            <span>Price: $150.00</span>\
+            <a class="add-to-cart" href="#">Add To Cart</a>\
+        </div>\
+        <div id="product-101" class="product">\
+            <img src="images/tennis.png">\
+            <h3 class="title"><a href="#">Product 102</a></h3>\
+            <span>Price: $120.00</span>\
+            <a class="add-to-cart" href="#">Add To Cart</a>\
+        </div>\
+        <div id="product-101" class="product">\
+            <img src="images/basketball.png">\
+            <h3 class="title"><a href="#">Product 103</a></h3>\
+            <span>Price: $90.00</span>\
+            <a class="add-to-cart" href="#">Add To Cart</a>\
+        </div>\
+        <div id="product-101" class="product">\
+            <img src="images/table-tennis.png">\
+            <h3 class="title"><a href="#">Product 104</a></h3>\
+            <span>Price: $110.00</span>\
+            <a class="add-to-cart" href="#">Add To Cart</a>\
+        </div>\
+        <div id="product-101" class="product">\
+            <img src="images/soccer.png">\
+            <h3 class="title"><a href="#">Product 105</a></h3>\
+            <span>Price: $80.00</span>\
+            <a class="add-to-cart" href="#">Add To Cart</a>\
+        </div>\
+    ';
+$('#products').html(html);
+}
