bdca0bb0d416dc5debdc780e145173b8e55eec87
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-03-03 16:08:22 +0530

    Delete product from cart funcionality is added.

--- src/products.js
+++ src/products.js
@@ -4,30 +4,43 @@ var products = [{"id":101,"name":"Basket Ball","image":"basketball.png","price":
                 {"id":104,"name":"Table Tennis","image":"table-tennis.png","price":130},
                 {"id":105,"name":"Tennis","image":"tennis.png","price":100}];
 var cart = [];
-// var product = {};
-// var c101=0, c102=0, c103=0,c104=0, c105=0;
 
 $(document).ready(function(){
     displayProducts();
-    // console.log(c101,c102, c103, c104, c105);
-    $('.add-to-cart').click(function(){       // .product 
+    
+    $('.add-to-cart').click(function(){      
         var pid = $(this).attr("id");
-        // console.log(pid);
-        // counter(pid);
+        
         displayCart(pid);
     });
 
+    // $('.delete').click(function(){
+    //     console.log("delete clicked")
+    //     var pid = $(this).data('pid');
+    //     pDel(pid);
+    // });
+
+    $('#cart').on('click','.delete',function(){
+		console.log("delete clicked");
+		var pid = $(this).data('pid');
+	
+		pDel(pid);
+		
+		display();
+	  });
+
+
+      $("#qty").on("keyup",function(){
+        console.log($(this).val());
+        //this gives me complete value inserted by user 
+        });
+
+    $('#cart').on('keyup','.delete',function(){
+        var value = $(this).val();
+        console.log(value);
+    });
+
 });
-
-// function counter(pid){
-//     if(pid == 101) c101++;
-//     else if(pid == 102) c102++;
-//     else if(pid == 103) c103++;
-//     else if(pid == 104) c104++;
-//     else if(pid == 105) c105++;
-
-//     console.log(c101,c102, c103, c104, c105);
-// }
 
 function displayProducts(){
     html = "";
@@ -85,18 +98,8 @@ function displayCart(pid){
 
 //    ********* fetching product to cart
     fetchProduct(pid);
-    // console.log(product.name);              // working fine
-
-//    ********* adding product to cart
-    // addProduct2cart(pid);
-    // console.log(cart);              // Not working fine*****
-
-
-//    ********* Display product cart
-    console.log(cart.length);
-
-    for(var i=0; i<cart.length; i++){
-        // console.log(cart[i].id);
+    
+    for(var i=0; i<cart.length; i++){               
         html += '<tr><td>'+cart[i].id+'</td><td>'+cart[i].name+'</td><td><input type="number" id="qty" name="qty" min="1" value="'+cart[i].qty+'"></td><td><a href="#" class="delete" data-pid='+cart[i].id+'>X</a></td></tr>';
     }   
 
@@ -107,71 +110,75 @@ function displayCart(pid){
 
 }
 
-function fetchProduct(pid){
-// console.log("pid in fetch product"+ pid);
-    var product = {};
-    for(var i=0; i<products.length; i++){
-        if(products[i].id == pid){
-            var qnty;
-            // ****  select the qnty counter  
-            // if(pid == 101) {qnty = c101;}
-            // else if(pid == 102) {qnty = c102;}
-            // else if(pid == 103) {qnty = c103;}
-            // else if(pid == 104) {qnty = c104;}
-            // else if(pid == 105) {qnty = c105;}
-            
+function pDel(pid){
+    console.log("inside pDel()");
+	for(var i=0; i<cart.length; i++){
+		if(cart[i].id == pid){//console.log(cart[i].id+" "+pid);
+			cart.splice(i,1);
+		}
+	}
+}
 
+function display(){
+    // console.log(pid);
 
-            product.id = products[i].id;
-            product.name = products[i].name;
-            // product.image = products[i].image;
-            // product.price = products[i].price;
-            product.qty = qnty;
-        }
-       
-    }
-    // console.log("the product is :"+ product.id+product.name+ product.qty);
+    html = "";
+    html += '<label for="Cart">Cart</label><br>';
+    html += '<table>\
+    <thead>\
+        <tr>\
+            <th>Product ID</th>\
+            <th>Product(s)</th>\
+            <th>Quantity</th>\
+            <th>Remove</th>\
+        </tr>\
+    </thead>\
+    <tbody>\
+    ';
+
+//    ********* fetching product to cart
+    // fetchProduct(pid);
+    
+    for(var i=0; i<cart.length; i++){               
+        html += '<tr><td>'+cart[i].id+'</td><td>'+cart[i].name+'</td><td><input type="number" id="qty" name="qty" min="1" value="'+cart[i].qty+'"></td><td><a href="#" class="delete" data-pid='+cart[i].id+'>X</a></td></tr>';
+    }   
+
+    html += '</tbody> </table>';
+
+    $('#cart').html(html);
 
 
 }
 
-function addProduct2cart(pid){
-   
-    if(product.id == pid){
-        if(isExists(product.id)){
-            console.log("product already exists")
-        }
-        else{
-            // console.log("inside else part in isExists Check");
-            console.log(cart.length);
-            console.log("product id is : "+product.id);
-            cart.push(product);
-            // cart[cart.length] = product;
-            console.log("cart is "+cart[0].id);
-            console.log(cart.length);
-        }
-        
-    }
 
+
+
+function fetchProduct(pid){
+    var product = {id: 0,name : "0", qty : 1};
+    
+    for(var i=0; i<products.length; i++){
+        if(products[i].id == pid){         
+            product.id = products[i].id;
+            product.name = products[i].name;            
+        }       
+    } 
+    addProduct2cart(product);
+}
+
+function addProduct2cart(product){
+    if(isExists(product.id)){
+    }
+    else {
+        cart.push(product);
+    }
 }
 
 function isExists(pid){
-    // if(pid == 101){
-    //     if(c101>1)
-    //     {c101++; 
-    //     return true;}}
-    // else if(pid == 102) {if(c102>1)
-    //     {c102++; 
-    //     return true;};}
-    // else if(pid == 103) {if(c103>1)
-    //     {c103++; 
-    //     return true;}}
-    // else if(pid == 104) {if(c104>1)
-    //     {c104++; 
-    //     return true;}}
-    // else if(pid == 105) {if(c105>1)
-    //     {c105++; 
-    //     return true;}}
-    // else
-    // return false;
+    for(var i=0; i<cart.length; i++){
+        if(cart[i].id == pid){
+            cart[i].qty += 1;
+            return true;
+        }
+    }
+    return false;
 }
